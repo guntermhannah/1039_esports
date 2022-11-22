7d3553fc20ae42db969e173498526a69a095d9be
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-22 13:02:26 -0700

    dropped matches dropped from matches.csv

--- 1039_esports/clean_data.py
+++ 1039_esports/clean_data.py
@@ -3,7 +3,9 @@ import pandas as pd
 import os
 
 def clean_player_data():
-    """Cleans the players data in the players.csv file, returns a clean dataframe"""
+    """Cleans the players data in the players.csv file, returns a clean dataframe. 
+    Drops any player data that does not have corresponding match in matches.csv"""
+
     df = pd.read_csv(os.path.join("data","players.csv"))
     
     # drop duplicates
@@ -15,5 +17,15 @@ def clean_player_data():
 
     # missing data -- missing account_id means it was a bot, not a player, drop those rows
     df_clean = df_clean.dropna()
+
+    # drop player data that has been dropped from matches.csv
+    matches = pd.read_csv(os.path.join("data","matches.csv"))
+    matches_list = matches["match_id"]
+
+    for i in range(len(df)):
+        if df[["match_id"]][i] not in matches_list:
+            df_clean.drop([i], inplace = True)
+
+
 
     return df_clean
