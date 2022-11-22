f7617838d202e184f33c9c4bc97e4016a56982b1
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-22 11:09:46 -0700

    bug fixes

--- 1039_esports/transform_data.py
+++ 1039_esports/transform_data.py
@@ -9,10 +9,9 @@ def transform_data():
 
     # retrieve matches data, append to matches csv
     matches_data = api.fetch_matches()
-    matches_data.to_csv(os.path.join("1039_esports","data", "matches.csv"),
+    matches_data.to_csv(os.path.join("data", "matches.csv"),
                         mode = "a", index = False, header=False)
 
-    player_data = []
     # loop through the matches data to get specific match infromation
     for i in range(len(matches_data)):
 
@@ -20,7 +19,12 @@ def transform_data():
         match_id = matches_data['match_id'][i]
         player_stats_data = api.fetch_stats_data(match_id)
 
+        if type(player_stats_data) == str:
+            continue 
+
         # append player data to players
-        pd.DataFrame(player_stats_data).to_csv(os.path.join("1039_esports","data", "players.csv"),
+        player_stats_data.to_csv(os.path.join("data", "players.csv"),
                                                             mode = "a", index = False,
                                                             header=False)
+
+transform_data()
