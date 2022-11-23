dab8a800a478c53457518198ad56bdfebaf7ee5b
Kenny PC <kennychavez.mtl@gmail.com>
2022-11-23 15:28:37 -0500

    added info_collector_script to retrieve data continuously

--- 1039_esports/retrieve_data.py
+++ 1039_esports/retrieve_data.py
@@ -7,8 +7,13 @@ import time
 def info_collector_script():
     "this method is a script to retrieve data from the api continuously"
     "specs: 60 calls/min"
-    start_time = time.time()
-    transform_data()
-    print("--- %s seconds ---" % (time.time() - start_time))
+    
+    while True:
+        try: 
+            transform_data()
+            
+        except:
+            continue
+    
 
 info_collector_script()
