d0fdaea868c3c6023231a45012292f58d12bb563
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-23 11:46:57 -0700

    renaming

---
+++ 1039_esports/get_data.py
@@ -0,0 +1,78 @@
+import requests
+import os
+import pandas as pd
+
+api_key = os.environ["API_KEY"]
+
+class Dota2Api():
+
+    def __init__(self):
+        self.token = os.environ.get('API_KEY')
+        self.url = 'https://api.opendota.com/api/'
+
+
+    def fetch_matches(self):
+        "This method gets random matches"
+        # make API call
+        url = "https://api.opendota.com/api/publicMatches"
+        params = {"api_key": api_key}
+
+        response = requests.get(url, params)
+        # response = requests.get(url)
+
+        # check api call was successful
+        if response.status_code != 200:
+            return "Error"
+
+        # parse results
+        data = pd.DataFrame(response.json())
+        stats = ["match_id", "radiant_win", "duration", "avg_mmr", "game_mode", "start_time"]
+        stats_filtered = data[[stat for stat in stats]]
+
+        return stats_filtered
+
+    def fetch_stats_data(self, match_id):
+        "This method gets stats data using a specified match id"
+        # make API call
+        url = f"https://api.opendota.com/api/matches/{match_id}"
+        params = {"api_key": api_key}
+
+        response = requests.get(url, params)
+        # response = requests.get(url)
+
+        # check api call was successful
+        if response.status_code != 200:
+            return "Error"
+
+        player_data = pd.DataFrame(response.json()["players"])
+
+        #filter player data by relevent stats
+        stats_keys = ['match_id', 'hero_id', 'account_id',
+                    'net_worth', 'kills', 'deaths','hero_damage',
+                    'gold_per_min', 'assists', 'xp_per_min', 
+                    'last_hits', 'tower_damage',
+                    'obs_placed', 'roshans_killed', 'isRadiant','win'
+        ]
+
+        data = player_data[[stat for stat in stats_keys]]
+
+        return data
+
+
+    def get_player_data(self, account_id):
+        "This method retrieves the 20 last games for a given player account id"
+
+        # making the api request to get the json data of the last 20 games
+        response = requests.get(self.url + f'/players/{account_id}/recentMatches').json()
+
+        # deciding the keys we are going to use
+        player_keys = ['match_id', 'radiant_win', 'duration', 'game_mode', 'lobby_type',
+                       'hero_id', 'kills', 'deaths', 'assists', 'average_rank',
+                       'xp_per_min', 'gold_per_min', 'hero_damage', 'tower_damage',
+                       'last_hits']
+
+        # turning the response into a dataframe and keeping only the keys we need
+        player_data = pd.DataFrame(response)
+        data = player_data[[stat for stat in player_keys]]
+
+        return data
---
+++ 1039_esports/transform_data.py
@@ -0,0 +1,54 @@
+from get_data import Dota2Api
+import pandas as pd
+import os
+
+api = Dota2Api()
+
+def transform_data():
+    "This method retrieves data from matches and statistics and outputs it to a csv file"
+
+    # retrieve matches data, append to matches csv
+    matches_data = api.fetch_matches()
+    matches_data.to_csv(os.path.join("data", "matches.csv"),
+                        mode = "a", index = False, header=False)
+
+    # loop through the matches data to get specific match infromation
+    for i in range(len(matches_data)):
+
+        # loop through specific match data and get player data
+        match_id = matches_data['match_id'][i]
+        player_stats_data = api.fetch_stats_data(match_id)
+
+        if type(player_stats_data) == str:
+            continue
+
+        # append player data to players
+        player_stats_data.to_csv(os.path.join("data", "players.csv"),
+                                                            mode = "a", index = False,
+                                                            header=False)
+
+
+def transform_player_data(account_id):
+    """
+    This method retrieves data from a players recent id, transforms it into
+    and then outputs it to a csv file
+    """
+
+    # we make a call to the get_data method to retrieve the recent matches
+    player_df = api.get_player_data(player_id)
+
+    # we also want to transform some data to usable data like kills to kills per min
+    keys_to_transform = ['kills', 'deaths', 'assists', 'hero_damage', 'tower_damage',
+                         'last_hits']
+    for key in keys_to_transform:
+
+        # we go through every row and replace the specific key value with its value/duration
+        player_df[key] = player_df.apply(lambda row: 60*row[key]/row['duration'], axis = 1)
+
+        # we also change the name of the column to the key_per_min format
+        player_df = player_df.rename(columns={key: f'{key}_per_min'})
+
+    # append player data to players
+    player_df.to_csv(os.path.join("1039_esports", "data", "single_player_data.csv"),
+                                                        mode = "a", index = False,
+                                                        header=False)
