c787bd637a6811257cc155996ecd67b7661ca0b9
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-23 12:10:13 -0700

    retrieved data

--- 1039_esports/decision_tree.py
+++ 1039_esports/decision_tree.py
@@ -6,9 +6,13 @@ import numpy as np
 
 
 def tree_model(max_depth = None, min_samples_split = 2, min_samples_leaf = 1, max_features = None):
+        # retrieve data and 
         df = pd.merge(clean_matches_data(), clean_player_data(), on="match_id")
 
-        df.drop
+        X = df.drop(columns = ["match_id", "hero_id", "account_id"])
+        y = df["hero_id"]
+
+
         tree = DecisionTreeClassifier(max_depth=max_depth, 
                         min_samples_split=min_samples_split, 
                         min_samples_leaf=min_samples_leaf,
@@ -16,6 +20,6 @@ def tree_model(max_depth = None, min_samples_split = 2, min_samples_leaf = 1, ma
 
 
 
-        print(df)
+        print(X, y)
 
 tree_model()
