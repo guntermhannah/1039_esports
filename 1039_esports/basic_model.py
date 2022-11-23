cd1ca7b0ef021ea797b5d85d4d6f826cb71979a5
Hannah Gunter <guntermhannah@gmail.com>
2022-11-23 12:53:46 -0400

    basic KNN model

--- 1039_esports/basic_model.py
+++ 1039_esports/basic_model.py
@@ -1,25 +1,9 @@
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import cross_validate
-from matches_clean import clean_matches_data, clean_player_data
-import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
 
-# match_id,hero_id,account_id,net_worth,kills,deaths,hero_damage,gold_per_min,assists,xp_per_min,last_hits,tower_damage,obs_placed,roshans_killed,isRadiant,win
-# match_id,radiant_win,duration,avg_mmr,game_mode,start_time
+def model_KNN(X_train, y_train, n_neighbors=2):
 
-def model_KNN():
+    knn_model = KNeighborsClassifier(n_nieghbors=n_neighbors)
 
-    # merge the datasets
-    df = pd.merge(clean_matches_data(), clean_player_data(), on='match_id')
+    knn_model.fit(X_train, y_train)
 
-    X = df.drop(columns=['match_id', 'hero_id', 'account_id', 'isRadiant', 'win', 'radiant_win'])
-    y = df['hero_id']
-
-    knn_model = KNeighborsRegressor()
-
-    cv_results = cross_validate(knn_model, X, y)
-
-    base_knn = cv_results['test_score'].mean()
-
-    return base_knn
-
-model_KNN()
+    return knn_model
