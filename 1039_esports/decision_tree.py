f8d990a76b7beba1b0b6a6cde0392fef5d06dc14
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-23 12:12:54 -0700

    deleted old file

--- 1039_esports/decision_tree.py
+++ 1039_esports/decision_tree.py
@@ -5,21 +5,14 @@ import pandas as pd
 import numpy as np
 
 
-def tree_model(max_depth = None, min_samples_split = 2, min_samples_leaf = 1, max_features = None):
-        # retrieve data and 
-        df = pd.merge(clean_matches_data(), clean_player_data(), on="match_id")
+def tree_model(X,y,max_depth = None, min_samples_split = 2, min_samples_leaf = 1, max_features = None):
 
-        X = df.drop(columns = ["match_id", "hero_id", "account_id"])
-        y = df["hero_id"]
-
-
+        # instantiate model
         tree = DecisionTreeClassifier(max_depth=max_depth, 
                         min_samples_split=min_samples_split, 
                         min_samples_leaf=min_samples_leaf,
                         max_features=max_features)
 
+        tree.fit(X, y)
 
-
-        print(X, y)
-
-tree_model()
+        return tree
