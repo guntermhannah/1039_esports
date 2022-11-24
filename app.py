0ba3e1faa01d7cffda7792e1f51c034e45f4d774
Kenny Chavez <kennychavez.mtl@gmail.com>
2022-11-24 11:00:33 -0500

    user interface ready for MVP

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import random
 import streamlit as st
 import numpy as np
 import pandas as pd
 
 # streamlit run app.py
-
 # Page structure
 st.set_page_config(
     page_title="Dota2 Player Statistics",
@@ -11,6 +11,8 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="auto"
 )
+st.image('1039_esports/data/home_page_img.jpeg')
+
 
 # Sidebar
 st.sidebar.markdown("# Navigation")
@@ -18,38 +20,45 @@ st.sidebar.markdown("# Navigation")
     # copy the corresponding information into each page
 
 # Page title and description
-st.markdown("""# Dota2 Player Statistics 🕹️""")
-st.text(""" Description """)
+st.markdown("""# Dota2 Win Predictor 🕹️""")
+st.text(""" We predict the outcome of the match between you and an opponent """)
 
 # Explanation of page
 st.markdown("""## ❓ How does it work ❓ """)
-st.text(""" Explanation """)
+st.text(""" Using the OpenDota Api, we retrieve your last matches as well as the
+opponents last matches and predict who will win""")
 
 st.markdown("""## ❗ Get started ❗ """)
 
-# Propt user to enter an account_id
-account_id = st.text_input("Enter your Account ID: ")
+# Prompt user to enter an account_id
+user_id = st.text_input("Enter your Account ID: ")
+opps_id = st.text_input("Enter your Opponent's Account ID: ")
 
 # Check if the ID is valid/exists
-'''
-if valid_id:
-    st.success(f'Account ID ', account_id,' found')
+# '''
+# if valid_id:
+#     st.success(f'Account ID ', account_id,' found')
+# else:
+#     st.error(f'Account ID ', account_id,' not found. Check your submission and try again')
+# '''
+
+# we will replace this block with the actual method later
+def get_user_win_rate(account_id):
+    return account_id
+# we retrieve the user win rate for the user and the opponent
+user_wr = get_user_win_rate(user_id)
+opps_wr = get_user_win_rate(opps_id)
+
+# if user wins
+if user_wr > opps_wr:
+    st.write("You have a higher probability of winning")
 else:
-    st.error(f'Account ID ', account_id,' not found. Check your submission and try again')
-'''
+    st.write("The opponent has a higher probability of winning")
 
-# Get the user data and display stats in a table
-    # Index being matches, columns being important stats
-def get_user_data(account_id: str)->pd.DataFrame:
-    # Example placeholder
-    return pd.DataFrame(
-        np.random.randn(10,10),
-        columns=('col %d' % i for i in range(10))
-    )
 
-user_data = get_user_data(account_id)
-st.write("Data for user: ", account_id)
-st.write(user_data.head())
+# user_data = get_user_data(account_id)
+# st.write("Data for user: ", account_id)
+# st.write(user_data.head())
 
 # Return the processed statistics on the user's data
 def get_user_stats()->pd.DataFrame:
@@ -75,51 +84,51 @@ def get_rank_stats(rank:int)->pd.DataFrame:
 
 # Comparison
 
-st.markdown("""## Compare your statistics with other ranks """)
+# st.markdown("""## Compare your statistics with other ranks """)
 
 
 
-col1_a, col2_a, col3_a = st.columns(3)
+# col1_a, col2_a, col3_a = st.columns(3)
 
-user_selected_rank = col1_a.selectbox("Select a rank for comparison: ",
-                                    ('Herald (0-769)',
-                                    'Guardian (770-1539)',
-                                    'Crusader (1540-2309)',
-                                    'Archon (2310-3079)',
-                                    'Legend (3080-3849)',
-                                    'Ancient (3850-4619)',
-                                    'Divine (4620-5420+)',
-                                    'Immortal (∽6000+)'))
+# user_selected_rank = col1_a.selectbox("Select a rank for comparison: ",
+#                                     ('Herald (0-769)',
+#                                     'Guardian (770-1539)',
+#                                     'Crusader (1540-2309)',
+#                                     'Archon (2310-3079)',
+#                                     'Legend (3080-3849)',
+#                                     'Ancient (3850-4619)',
+#                                     'Divine (4620-5420+)',
+#                                     'Immortal (∽6000+)'))
 
-user_selected_hero = col3_a.selectbox("Select your hero: ",
-                                  ('Pudge',
-                                   'Juggernaut',
-                                   'Sniper',
-                                   'Lion',
-                                   'Phantom Assassin',
-                                   'Shadow Fiend',
-                                   'Witch Doctor',
-                                   'Ogre Magi',
-                                   'Zeus',
-                                   'Invoker'))
+# user_selected_hero = col3_a.selectbox("Select your hero: ",
+#                                   ('Pudge',
+#                                    'Juggernaut',
+#                                    'Sniper',
+#                                    'Lion',
+#                                    'Phantom Assassin',
+#                                    'Shadow Fiend',
+#                                    'Witch Doctor',
+#                                    'Ogre Magi',
+#                                    'Zeus',
+#                                    'Invoker'))
 
-user_selected_role = col2_a.selectbox("Select your role: ",
-                                    ("Carry",
-                                    "Tank",
-                                    "Support"))
+# user_selected_role = col2_a.selectbox("Select your role: ",
+#                                     ("Carry",
+#                                     "Tank",
+#                                     "Support"))
 
-col1_b, col2_b = st.columns(2)
+# col1_b, col2_b = st.columns(2)
 
-col1_b.write("Stats for user:")
-col1_b.write(get_user_stats())
+# col1_b.write("Stats for user:")
+# col1_b.write(get_user_stats())
 
-col2_b.write("Comparison stats:")
-col2_b.write(get_rank_stats())
+# col2_b.write("Comparison stats:")
+# col2_b.write(get_rank_stats())
 
 # Recommendations
     # Areas most affected
     # Areas least affected
     # Areas most important
 
-st.markdown("""## Recommendations """)
-st.text(""" Recommendations """)
+# st.markdown("""## Recommendations """)
+# st.text(""" Recommendations """)
