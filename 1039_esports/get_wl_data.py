e259d98f7db53ac1693dc7e55feab3db54c55af8
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-24 10:45:22 -0700

    created method for getting player pairs, created csv of player pairs

--- 1039_esports/get_wl_data.py
+++ 1039_esports/get_wl_data.py
@@ -37,4 +37,46 @@ def build_wl_dataset():
 
     df.to_csv(os.path.join("data", "wl_data.csv"), mode = "a", index = False, header=True)
 
-build_wl_dataset()
+
+def get_match_pairs():
+    """returns dataframe of match_ids, with player account_id, opponent account_id, and who won"""
+    # retrieve player data
+    player_data = clean_player_data()
+
+    # group player data by match_id, take the first account of winning and losing teams
+    tmp = player_data.groupby(["match_id", "isRadiant"]).first()
+    
+    #create df of pairs of winners and losers
+    games = []
+    for match, new_df in tmp.groupby(level=[0]):
+        game = {"match_id": 0,
+                "player": 0,
+                "opponent": 0,
+                "winner": 0}
+        game["match_id"] = match
+        if len(new_df) !=2:
+            continue
+        
+        # get player account_id
+        player_account_id = str(int(new_df.iloc[0,1]))
+        if len(player_account_id)<9:
+            player_account_id = player_account_id + "0"*(9-len(player_account_id))
+        game["player"] = player_account_id
+
+        # get opponent account_id
+        opponent_account_id = str(int(new_df.iloc[1,1]))
+        if len(opponent_account_id)<9:
+            opponent_account_id = opponent_account_id + "0"*(9-len(opponent_account_id))
+        game["opponent"] = opponent_account_id
+
+        # get winner info
+        if new_df.iloc[0,-1] == 1:
+            game["winner"] = player_account_id
+        else:
+            game["winner"] = opponent_account_id
+        games.append(game)
+
+    pd.DataFrame(games).to_csv(os.path.join("data", "player_pairs.csv"), mode = "a", index = False, header=True)
+
+
+get_match_pairs()
