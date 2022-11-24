311a995e5abae9108f885abd9bfd45f61c19ab06
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-24 14:52:22 -0700

    decision tree model

--- 1039_esports/decision_tree.py
+++ 1039_esports/decision_tree.py
@@ -1,21 +1,28 @@
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_validate
-from matches_clean import clean_matches_data, clean_player_data
 import pandas as pd
 import numpy as np
+import os
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
-def tree_model(max_depth = None, min_samples_split = 2, min_samples_leaf = 1, max_features = None):
-        df = pd.merge(clean_matches_data(), clean_player_data(), on="match_id")
+def decision_tree():
+    """returns tree model fit to data"""
 
-        df.drop
-        tree = DecisionTreeClassifier(max_depth=max_depth, 
-                        min_samples_split=min_samples_split, 
-                        min_samples_leaf=min_samples_leaf,
-                        max_features=max_features)
+    # retrieve data
+    df = pd.read_csv(os.path.join("data", "player_pairs_stats.csv"))
+    df["player_win"] = (df["player"] == df["winner"]).apply(lambda x:int(x))
 
+    X = df.drop(columns = ["match_id","player", "opponent","winner", "player_win"])
+    y = df["player_win"]
 
+    X_train, X_test, y_train, y_test = train_test_split()
 
-        print(df)
+    
 
-tree_model()
+    # tree model
+    tree = DecisionTreeClassifier()
+    tree.fit
+
+    return tree.predict(X_test), y_test
+
+print(decision_tree()) 
