0d41e65edc16d51077d604874fe0cc73790b8729
Hannah Gunter <guntermhannah@gmail.com>
2022-11-24 18:45:19 -0400

    Added methods for base model

--- 1039_esports/base_model.py
+++ 1039_esports/base_model.py
@@ -1,39 +1,67 @@
 import pandas as pd
 import os
-from matches_clean import clean_player_data
+import numpy as np
 
-def get_win_ratio():
+def get_player_ratio(account_id:int)->float:
 
-    # Turn csv file into DataFrame
-    df = pd.read_csv(os.path.join('data', 'wl_data.csv'))
+    # Get the data for player_pairs and wl_data
+    df_wl = pd.read_csv(os.path.join('data', 'wl_data.csv'))
 
-    # Drop duplicates of account_id
-    df.drop_duplicates(subset=['account_id'], inplace=True)
+    # Remove rows where total == 0
+    df_wl = df_wl[df_wl['total'] != 0]
 
-    # Create ratio column
-    df['wl_ratio'] = df['win'] / df['total']
+    # Check if account_id is in the wl_data.csv
+    if account_id in df_wl['account_id'].values:
+        # Find that row
+        row = df_wl[df_wl['account_id'] == account_id]
+        # Calculate ratio
+        ratio = int(row['wins']) / int(row['total'])
+        return float(ratio)
 
-    # Sum ratio column
-    win_rate = df['wl_ratio'].sum() / len(df['wl_ratio'])
-
-    return win_rate
+    # Return -1.0 value if not found in csv
+    return -1.0
 
 
+def column_e():
 
-def win_ratio():
+    df_pairs = pd.read_csv(os.path.join('data', 'player_pairs.csv'))
 
-    # Turn csv file into DataFrame
-    df = jasmine_get()
+    # y_pred = if player wins
+    y_pred = []
+    y_true = []
 
-    # Grab account_id
-    player1 = df['player_1'][0]
-    player2 = df['player_2'][0]
+    for row in range(0, len(df_pairs)):
 
-    # Determine how many wins that account_id has against the other account_id
-    player1_wins = df.won_id.value_counts().player1
-    player2_wins = df.won_id.value_counts().player2
+        # Get the account_id of player and opponent
+        player_id = int(df_pairs['player'][row])
+        opponent_id = int(df_pairs['opponent'][row])
+        winner_id = int(df_pairs['winner'][row])
 
-    # Determine the win percentage of player1
-    ratio = player1_wins / player2_wins
+        # Get the ratios of each player
+        player_ratio = get_player_ratio(player_id)
+        opponent_ratio = get_player_ratio(opponent_id)
 
-    return ratio
+        # Find prediction only if both players have ratio from wl_data
+        if (player_ratio > 0) and (opponent_ratio > 0):
+
+            # Determine if player won
+            if (player_ratio > opponent_ratio):
+                y_pred.append(1)
+            else:
+                y_pred.append(0)
+
+            y_pred = np.array([y_pred])
+
+            if (player_id == winner_id):
+                y_true.append(1)
+            else:
+                y_true.append(0)
+
+            y_true = np.array([y_true])
+
+        else:
+            pass
+
+    pass
+
+column_e()
