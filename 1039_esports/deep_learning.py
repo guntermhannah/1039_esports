000a40a51a9edd11372cb413426d8f5f715f6ea4
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-25 08:45:31 -0700

    deep learning model

--- 1039_esports/deep_learning.py
+++ 1039_esports/deep_learning.py
@@ -1,9 +1,17 @@
 from tensorflow.keras import Sequential, layers
 
 
-def init_model(X,y):
-    # model architecture
-    model = Sequential()
-    model.add(layers.Dense(20, activation = "relu", input_dim))
+def init_model():
+    """instantiates, compiles, and fits an NN to the data"""
 
+    # retrieve data
+    df = pd.read_csv(os.path.join("data", "player_pairs_stats.csv"))
+    df["player_win"] = (df["player"] == df["winner"]).apply(lambda x:int(x))
+    print(df.shape)
+
+    # # model architecture
+    # model = Sequential()
+    # model.add(layers.Dense(20, activation = "relu", input_dim))
+
+init_model()
 
