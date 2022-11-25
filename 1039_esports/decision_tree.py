9f8660c902d504efa52b7dad0d83e440402a4802
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-25 12:48:16 -0700

    decision tree merge

--- 1039_esports/decision_tree.py
+++ 1039_esports/decision_tree.py
@@ -3,29 +3,6 @@ import numpy as np
 import os
 
 from sklearn.tree import DecisionTreeClassifier
-<<<<<<< HEAD
-from sklearn.model_selection import train_test_split
-
-def decision_tree():
-    """returns tree model fit to data"""
-
-    # retrieve data
-    df = pd.read_csv(os.path.join("data", "player_pairs_stats.csv"))
-    df["player_win"] = (df["player"] == df["winner"]).apply(lambda x:int(x))
-
-    X = df.drop(columns = ["match_id","player", "opponent","winner", "player_win"])
-    y = df["player_win"]
-
-    X_train, X_test, y_train, y_test = train_test_split()
-
-    
-
-    # tree model
-    tree = DecisionTreeClassifier()
-    tree.fit
-
-    return tree.predict(X_test), y_test
-=======
 from sklearn.preprocessing import RobustScaler
 from sklearn.metrics import accuracy_score
 
@@ -50,6 +27,5 @@ def decision_tree(max_depth=None, min_samples_split=2, min_samples_leaf=1, max_f
     y_pred = tree.predict(X_test_scaled)
 
     return accuracy_score(y_pred, y_test)
->>>>>>> 53f890c88f85bcc9476cdf32d1d125289232138b
 
 print(decision_tree()) 
