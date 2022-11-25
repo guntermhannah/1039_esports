6b7c0ff90ee5b1f10be2b8dee1bfc50da73adf4a
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-25 11:54:52 -0700

    decision tree with 57% accuracy

--- 1039_esports/decision_tree.py
+++ 1039_esports/decision_tree.py
@@ -3,17 +3,16 @@ import numpy as np
 import os
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 from sklearn.metrics import accuracy_score
 
-from matches_clean import train_test_split
+from matches_clean import train_test_split_data
 
 def decision_tree(max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features=None):
     """returns tree model fit to data"""
 
     # train test split of data
-    X_train, X_test, y_train, y_test = train_test_split()
+    X_train, X_test, y_train, y_test = train_test_split_data()
 
     # scale the data
     scaler = RobustScaler()
@@ -27,6 +26,6 @@ def decision_tree(max_depth=None, min_samples_split=2, min_samples_leaf=1, max_f
 
     y_pred = tree.predict(X_test_scaled)
 
-    return tree, accuracy_score(y_test, y_pred)
+    return accuracy_score(y_pred, y_test)
 
 print(decision_tree()) 
