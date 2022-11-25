23efecfa718aa96bd979b33aa11d1e2999fba02a
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-25 15:44:14 -0700

    latest edition

--- 1039_esports/deep_learning.py
+++ 1039_esports/deep_learning.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 
 from tensorflow.keras import Sequential, layers
+from tensorflow.keras.callbacks import EarlyStopping
 
 from matches_clean import clean_player_pairs_data
 
@@ -14,24 +15,24 @@ def get_data():
     # returns cleaned player pair stat data
     df = clean_player_pairs_data()
 
-    # add wl data to the df
-    wl_data = pd.read_csv(os.path.join("data", "wl_data.csv"))
-    wl_clean = wl_data[wl_data["wins"] != "wins"]
-    wl_clean = wl_clean[wl_clean["total"] != "0"]
-    wl_clean = wl_clean.drop_duplicates(subset="account_id", keep="last")
-    wl_clean["account_id"] = wl_clean["account_id"].astype(int)
-    wl_clean["win_ratio"] = wl_clean["wins"].astype(int)/wl_clean["total"].astype(int)
+    # # add wl data to the df
+    # wl_data = pd.read_csv(os.path.join("data", "wl_data.csv"))
+    # wl_clean = wl_data[wl_data["wins"] != "wins"]
+    # wl_clean = wl_clean[wl_clean["total"] != "0"]
+    # wl_clean = wl_clean.drop_duplicates(subset="account_id", keep="last")
+    # wl_clean["account_id"] = wl_clean["account_id"].astype(int)
+    # wl_clean["win_ratio"] = wl_clean["wins"].astype(int)/wl_clean["total"].astype(int)
 
-    merged = df.merge(wl_clean[["account_id", "win_ratio"]], left_on = "opponent", right_on = "account_id")
-    merged["player_win_ratio"] = merged["win_ratio"]
-    merged = merged.drop(columns= ["account_id", "win_ratio"])
-    merged = merged.merge(wl_clean[["account_id", "win_ratio"]], left_on = "opponent", right_on = "account_id")
-    merged["opponent_win_ratio"] = merged["win_ratio"]
-    merged = merged.drop(columns= ["account_id", "win_ratio"])
+    # merged = df.merge(wl_clean[["account_id", "win_ratio"]], left_on = "player", right_on = "account_id")
+    # merged["player_win_ratio"] = merged["win_ratio"]
+    # merged = merged.drop(columns= ["account_id", "win_ratio"])
+    # merged = merged.merge(wl_clean[["account_id", "win_ratio"]], left_on = "opponent", right_on = "account_id")
+    # merged["opponent_win_ratio"] = merged["win_ratio"]
+    # merged = merged.drop(columns= ["account_id", "win_ratio"])
 
     # separate data and target
-    X = merged.drop(columns = ["match_id", "player", "opponent", "winner", "player_win", "player_tower_damage", "player_roshans_killed", "player_obs_placed", "opponent_tower_damage","opponent_roshans_killed","opponent_obs_placed"])
-    y = merged["player_win"]
+    X = df.drop(columns = ["match_id", "player", "opponent", "winner", "player_win", "player_tower_damage", "player_roshans_killed", "player_obs_placed", "opponent_tower_damage","opponent_roshans_killed","opponent_obs_placed"])
+    y = df["player_win"]
 
     # train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
@@ -50,14 +51,23 @@ def init_model(X, y):
     # model architecture
     model = Sequential()
     model.add(layers.Dense(30, activation = "relu", input_dim = 18))
-    model.add(layers.Dense(30, activation = "relu"))
+    model.add(layers.Dense(25, activation = "relu"))
+    model.add(layers.Dropout(rate=0.2))
+    model.add(layers.Dense(20, activation = "relu"))
     model.add(layers.Dense(1, activation = "sigmoid"))
 
     # compile model
     model.compile(loss = "binary_crossentropy", optimizer="adam", metrics ="accuracy")
 
     # fit model
-    history = model.fit(X,y, batch_size = 32, validation_split = 0.3, epochs = 300, verbose = True)
+    es = EarlyStopping(patience = 20, restore_best_weights = True)
+
+    history = model.fit(X,y, 
+                        batch_size = 32, 
+                        validation_split = 0.3,
+                        epochs = 300, 
+                        callbacks = es,
+                        verbose = True)
 
     return history, model
 
