de3baeff45cb2c9fdae61cad3965b419bfbe0ee0
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-26 10:00:31 -0700

    new data

--- 1039_esports/build_training_set.py
+++ 1039_esports/build_training_set.py
@@ -1,32 +1,44 @@
 import pandas as pd
 import os
 import time
+import requests
 
 from matches_clean import clean_player_data, clean_matches_data
 from transform_data import average_player_data
-from get_wl_data import get_wl_data
 
 
 def wrapper(x):
     if type(average_player_data(x)) == str:
-        return {'deaths_per_min':None, 
-                'assists_per_min': None, 
-                'tower_damage_per_min': None, 
-                'xp_per_min': None, 
-                'gold_per_min': None, 
-                'kills_per_min': None,
-                'hero_damage_per_min': None, 
-                'last_hits_per_min': None
-        }
+        empty_df ={'deaths_per_min':0, 
+                'assists_per_min': 0, 
+                'tower_damage_per_min': 0, 
+                'xp_per_min': 0, 
+                'gold_per_min': 0, 
+                'kills_per_min': 0,
+                'hero_damage_per_min': 0, 
+                'last_hits_per_min': 0}
+        return pd.DataFrame([empty_df.values()], columns = empty_df.keys())
     else:
         return average_player_data(x)
 
 
+def get_wl_data(account_id):
+    """retrieves win/loss data for an individual player, returns dict"""
+
+    url = f"https://api.opendota.com/api/players/{account_id}/wl"
+    
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        return {"win":0, "lose": 0}
+
+    data = response.json()
+
+    return data
+
 def get_training_set():
     """returns dataframe of match_ids, with player and opponent account ids,
     winner, and player and opponent average stats over last 20 games"""
-    
-    print("Checkpoint 0")
 
     # retrieve player data and match data
     player_data = clean_player_data()
@@ -34,8 +46,6 @@ def get_training_set():
 
     # merge datasets
     df = player_data.merge(matches_data)
-
-    print("Checkpoint 1")
 
     # relevant features
     features = ['deaths_per_min', 
@@ -47,12 +57,10 @@ def get_training_set():
                 'hero_damage_per_min', 
                 'last_hits_per_min', 
     ]
-    print("Checkpoint 2")
 
     # group player data by match_id, take the first account of winning and losing teams
-    tmp = df[3000:].groupby(["match_id", "isRadiant"]).first()
+    tmp = df[3500:].groupby(["match_id", "isRadiant"]).first()
 
-    print("Checkpoint 3")
     # create df of pairs of winners and losers
     for match, new_df in tmp.groupby(level=[0]):
         game = {"match_id": 0,
@@ -84,8 +92,6 @@ def get_training_set():
         game["match_id"] = match
         if len(new_df) !=2:
             continue
-
-        print("Checkpoint 4")
             
         # get player account_id
         player_account_id = str(int(new_df.loc[(match, False),"account_id"]))
@@ -105,8 +111,7 @@ def get_training_set():
         else:
             game["winner"] = opponent_account_id
 
-        print("Checkpoint 5")
-                
+
         game_df = pd.DataFrame([game.values()], columns = game.keys())
         
         # drop data without per min stats
@@ -119,40 +124,31 @@ def get_training_set():
                                     ])
 
         # # get player and opponent average history
-        for feature in features:
-            game_df[f"player_{feature}"] = game_df["player"].apply(lambda x:wrapper(x)[feature])
-            game_df[f"opponent_{feature}"] = game_df["opponent"].apply(lambda x:wrapper(x)[feature])
-        
-        print("Checkpoint 6")
+        try:
+            for feature in features:
+                game_df[f"player_{feature}"] = game_df["player"].apply(lambda x:wrapper(x)[feature])
+                game_df[f"opponent_{feature}"] = game_df["opponent"].apply(lambda x:wrapper(x)[feature])
+        except TypeError:
+            continue
 
+
+        # get player and opponent wl data
         player_wl_data = get_wl_data(player_account_id)
+        if player_wl_data["win"] + player_wl_data["lose"] == 0:
+            continue
+        player_win_ratio = player_wl_data["win"]/(player_wl_data["lose"] + player_wl_data["win"])
+        game_df["player_win_ratio"] = player_win_ratio
 
-        print(player_wl_data)
+        opponent_wl_data = get_wl_data(opponent_account_id)
+        if opponent_wl_data["win"] + opponent_wl_data["lose"] == 0:
+            continue
+        opponent_win_ratio = opponent_wl_data["win"]/(opponent_wl_data["lose"] +opponent_wl_data["win"])
+        game_df["opponent_win_ratio"] = opponent_win_ratio
 
-        # # # merge wl data with player pair data
-        # player_wl_stats = wl_clean[wl_clean["account_id"] == player_account_id]
-        # opponent_wl_stats = wl_clean[wl_clean["account_id"] == opponent_account_id]
 
-
-
-        # merged = game_df.merge(wl_clean[["account_id", "win_ratio"]], left_on = "player", right_on = "account_id")
-        # merged["player_win_ratio"] = merged["win_ratio"]
-        # merged = merged.drop(columns= ["account_id", "win_ratio"])
-        # merged = merged.merge(wl_clean[["account_id", "win_ratio"]], left_on = "opponent", right_on = "account_id")
-        # merged["opponent_win_ratio"] = merged["win_ratio"]
-        # merged = merged.drop(columns= ["account_id", "win_ratio"])
-
-        # print("Checkpoint 7")
-        # print(merged)
-
-        # merged_df = pd.DataFrame(merged)
         game_df.to_csv(os.path.join("data", "player_pairs_avg_stats.csv"), mode = "a", index = False, header=False)
 
-        print("Checkpoint 8")
-        print(game_df)
-
-
-        time.sleep(2)
+        time.sleep(4)
 
 
 get_training_set()
