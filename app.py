1cce0df1a1cfa09d00078f3e7ed892bd297ee0ad
Jasmine Zyp <jasminezyp@gmail.com>
2022-11-29 07:02:57 -0700

    update

--- app.py
+++ app.py
@@ -30,48 +30,50 @@ st.text(""" We predict the outcome of the match between you and an opponent """)
 # Explanation of page
 st.markdown("""## ❓ How does it work ❓ """)
 st.text(""" Using the OpenDota Api, we retrieve your last matches as well as the
-opponents last matches and predict who will win""")
+opponents last matches and predict who will win.""")
 
 st.markdown("""## ❗ Get started ❗ """)
 
+# User chooses their role (player/better)
+role = st.selectbox("Are you a player or a better?", ["Player 🕹️", "Better 💸"])
+if role == ""
+
+
 # Prompt user to enter an account_id
 account_id = st.text_input("Enter your Account ID: ")
+user_id = steam_id_finder(account_id)
+if not user_id:
+    st.error("The account ID is not a valid account ID, please try again")
 opps_account_id = st.text_input("Enter your Opponent's Account ID: ")
+opps_id = steam_id_finder(opps_account_id)
+if not opps_id:
+    st.error("The account ID is not a valid account ID, please try again")
 
-# if account_id and opps_account_id
-if account_id and opps_account_id != '':
-    # retrieve the steam32 id for both accounts and check if they exist
-    user_id, opps_id = steam_id_finder(account_id), steam_id_finder(opps_account_id)
-    if user_id and opps_id is not False:
-        st.success("The ID's entered are correct")
-    elif user_id and opps_id is False:
-        st.error("Both account ID's are not valid, please try again")
-    elif user_id is False:
-        st.error(f'The account ID entered is not a valid account id, please try again')
-    elif opps_id is False:
-        st.error(f"The opponent's account ID is not a valid account id, please try again")
 
+if user_id and opps_id:
     # if the id inputted are correct, we get their win rates
-    if user_id != '':
-        user_wr = get_wl_data(user_id)
-        opps_wr = get_wl_data(opps_id)
-        # since the method outputs 0,0 for faulty ids, we just give them an arbitrary number
-        try:
-            user_wr = user_wr['win'] / (user_wr['win'] + user_wr['lose'])
-        except Exception as e:
-            user_wr = 0
-        try:
-            opps_wr = user_wr['win'] / (user_wr['win'] + user_wr['lose'])
-        except Exception as e:
-            opps_wr = 0
+    user_wr = get_wl_data(user_id)
+    opps_wr = get_wl_data(opps_id)
+    # since the method outputs 0,0 for faulty ids, we just give them an arbitrary number
+    try:
+        user_wr = user_wr['win'] / (user_wr['win'] + user_wr['lose'])
+    except Exception as e:
+        user_wr = 0
+        st.error("Looks like this player hasn't played any games... try entering a different account ID")
+    try:
+        opps_wr = opps_wr['win'] / (opps_wr['win'] + opps_wr['lose'])
+    except Exception as e:
+        opps_wr = 0
+        st.error("Looks like this player hasn't played any games... try entering a different account ID")
 
-        # if user wins or loses
-        if user_wr > opps_wr:
-            st.write("You have a higher probability of winning")
-        elif user_wr == opps_wr:
-            st.write("You both have the same probability of winning")
-        else:
-            st.write("The opponent has a higher probability of winning")
+
+    # if user wins or loses
+    if user_wr > opps_wr:
+        st.write("You have a higher probability of winning")
+    elif user_wr == opps_wr:
+        st.write("You both have the same probability of winning")
+    else:
+        st.write("The opponent has a higher probability of winning")
 
 
 # ~~~~~~~~~~~~ misc ~~~~~~~~~~~~~~~~
